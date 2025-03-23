65fc85307032fb7167b9daa9453720f6bd38568d
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-03-24 01:42:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 # Fetch bot token from environment variables
-BOT_TOKEN = "your_bot_token_here"
+BOT_TOKEN = "1234567890:ABCDEF1234567890abcdef1234567890"  # Example
 WEBHOOK_URL = "https://your-app-name.koyeb.app/"  # Replace with your Koyeb app URL
